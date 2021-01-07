c4ce1c16402125cb5eeaecf241583f7f4198f24e
StunxFS <advancestunx@hotmail.com>
2021-01-07 15:21:08 -0400

    all: año de copyright actualizado

--- compiler/token/token.v
+++ compiler/token/token.v
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 StunxFS. All rights reserved. Use of this source code
+// Copyright (c) 2020-2021 StunxFS. All rights reserved. Use of this source code
 // is governed by an MIT license that can be found in the LICENSE file.
 module token
 
