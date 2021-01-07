a59ef506c9b284a13cf5f27c26afc17c14a6655b
StunxFS <advancestunx@hotmail.com>
2021-01-07 13:47:31 -0400

    compiler.about: arreglos menores

--- compiler/about/about.v
+++ compiler/about/about.v
@@ -4,27 +4,26 @@ module about
 
 import os
 
-pub const (
-	version 			= "0.1.0a0"
-	emitter_bin_version = "alpha0"
-	build_time			= '<unknown>'
-)
+const version 			  = "0.1.0"
+const emitter_bin_version = "0.1.0"
+const status			  = "alpha"
+const build_time		  = '<unknown>'
 
 pub fn help() {
-	program := os.args[0]
+	program := os.executable()
 	println("=================================================
-FokerScript | Mensaje de Ayuda | Work In Progress
+ZubatScript | Mensaje de Ayuda | Work In Progress
 =================================================
 Autor: StunxFS
 Fecha de compilación: $build_time
-Versión: $version
+Versión: $version $status
 
 Descripción:
-    Bienvenido al compilador oficial del proyecto FokerScript.
+    Bienvenido al compilador oficial del proyecto ZubatScript.
     Este compilador aún está bajo desarrollo, es decir, que todavía es un trabajo en progreso.
 
 Uso:
-    $program [opciones] archivo.fkr
+    $program [opciones] archivo.zubat
 
 Información básica sobre los backends:
     Versión del backend de binario: $emitter_bin_version
@@ -32,10 +31,10 @@ Información básica sobre los backends:
 
 Argumentos:
     archivo-fkr
-        Este argumento no se declara, sino que recibe el archivo de script .fkr que será
+        Este argumento no se declara, sino que recibe el archivo de script .zubat que será
         compilado.
         Ejemplo de uso:
-            $program miarchivo.fkr
+            $program miarchivo.zubat
 
 Opciones:
     -h, -a, help, ayuda
@@ -55,18 +54,18 @@ Opciones:
         Esta opción define el nombre de la bandera que se le pase, esto es muy útil para manejar
         código que se compila condicionalmente. (ej: #if DEFINED_2020)
         Ejemplo de uso:
-            $program -d DEFINED_2020 main_def_test.fkr
+            $program -d DEFINED_2020 main_def_test.zubat
 
     -o, -output
         Esta opción le especifica al compilador cómo debería llamarse el archivo de salida
         Ejemplo de uso:
-            $program -o micarpeta/miarchivo.rbh miarchivo.fkr
-    
+            $program -o micarpeta/miarchivo.rbh miarchivo.zubat
+
     -r, -rom
         Esta opción le dice al compilador el nombre de la ROM que se usará para insertar el
         script. OJO: Esta opción no se puede usar junto a -o, -output
         Ejemplo de uso:
-            $program -r mirom.gba miscript.fkr
+            $program -r mirom.gba miscript.zubat
 
     -g, -game
         Esta opción le especifica al compilador que ROM se usará para el script, por defecto
@@ -76,49 +75,49 @@ Opciones:
             frlf, fireredleafgreen: Pokémon FireRed/LeafGreen
             e, emerald: Pokémon Emerald
         Ejemplo de uso:
-            $program -g fr miarchivo.fkr
+            $program -g fr miarchivo.zubat
 
     -fast, -debug
         Estas opciones le dicen al compilador que nivel de optimización debe usar para generar
         el script, esto permite realizar un script que pueda ser feo y lento (-debug), o uno rápido
         y limpio (-fast). Por defecto el valor es -debug
         Ejemplo de uso:
-            $program -fast miarchivo.fkr
-            $program -debug miarchivo.fkr
-    
+            $program -fast miarchivo.zubat
+            $program -debug miarchivo.zubat
+
     -warns-are-errors
         Esta opción le dice al compilador que trate las advertencias como errores.
         Ejemplo de uso:
-            $program -warns-are-errors main_12.fkr
+            $program -warns-are-errors main_12.zubat
 
     -skip-warnings
         Esta opción hace que el compilador no muestre advertencias.
         Ejemplo de uso:
-            $program -skip-warnings main_14.fkr
-    
+            $program -skip-warnings main_14.zubat
+
     -verbose
         Esta opción hace que el compilador detalle cada paso que hace
         Ejemplo de uso:
-            $program -verbose main_98.fkr
-    
+            $program -verbose main_98.zubat
+
     -nocolor
         Esta opción hace que el compilador nunca use colores en los mensajes de error, advertencia, etc
         Ejemplo de uso:
-            $program -nocolor main_282.fkr
+            $program -nocolor main_282.zubat
 
     -color
         Esta opción hace que el compilador siempre use los colores en los mensajes de error, advertencia, etc
         Ejemplo de uso:
-            $program -color main_13.fkr
+            $program -color main_13.zubat
 
     -only-check-syntax
         Esta opción solo parsea el archivo dad, evitando el chequeo y la generación del programa, esta opción
         puede ser útil para aprender la sintaxis de FokerScript.
         Ejemplo de uso:
-            $program -only-check-syntax main_26.fkr
+            $program -only-check-syntax main_26.zubat
 
 Ejemplos de usos del programa:
-    $program -o mi_script_perron.inc -g rs -b decomp mi_script.fkr
-    $program -fast -skip-warnings -g rs mi_script.fkr
-    $program -debug -warns-are-errors -g rs mi_script.fkr")
+    $program -o mi_script_perron.inc -g rs -b decomp mi_script.zubat
+    $program -fast -skip-warnings -g rs mi_script.zubat
+    $program -debug -warns-are-errors -g rs mi_script.zubat")
 }
