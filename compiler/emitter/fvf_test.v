dbda01b232562bd9f174843ca716b85580943027
StunxFS <advancestunx@hotmail.com>
2021-01-17 19:47:23 -0400

    emitter.fvf: tests mejorados

--- compiler/emitter/fvf_test.v
+++ compiler/emitter/fvf_test.v
@@ -2,11 +2,24 @@
 // governed by an MIT license that can be found in the LICENSE file.
 module emitter
 
-fn test_fvf() {
-	mut fvf := new_fvf('
-80AB
+const vars = '80AB
 801B
-')
-	_ := fvf.get() or { panic(err) }
-	_ := fvf.get() or { panic(err) }
+'
+
+fn test_get() {
+	mut fvf := new_fvf(vars)
+	v1 := fvf.get() or { panic(err) }
+	v2 := fvf.get() or { panic(err) }
+	assert v1 == '80AB'
+	assert v2 == '801B'
+}
+
+fn test_free() {
+	mut fvf := new_fvf(vars)
+	v1 := fvf.get() or { panic(err) }
+	v2 := fvf.get() or { panic(err) }
+	assert v1 == '80AB'
+	assert v2 == '801B'
+	fvf.free(v1)
+	fvf.free(v2)
 }
